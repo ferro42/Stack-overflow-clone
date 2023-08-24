a8ba13f1b1aebe4d66d3a478a3039f7bd2945bb0
ferro42 <136361580+ferro42@users.noreply.github.com>
2023-08-25 01:47:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use('/answer', answerroutes)
 
 const PORT = process.env.PORT || 5000
 
-const DATABASE_URL = process.env/CONNNECTION_URL
+const DATABASE_URL = process.env/CONNECTION_URL
 
 mongoose.connect(DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, ()=> {console.log(`server running on port ${PORT}`)}))
